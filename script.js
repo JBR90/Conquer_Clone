3e2787a70f882fa9b73465d561309c836f7614e6
JBR90 <blakeage77@googlmail.com>
2021-04-26 15:26:34 +0100

    Basic template

---
+++ script.js
@@ -0,0 +1,51 @@
+// // Create a condition that targets viewports at least 768px wide
+// const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+// function handleTabletChange(e) {
+//   // Check if the media query is true
+//   if (e.matches) {
+//     // Then log the following message to the console
+//     console.log("Media Query Matched!");
+//   }
+// }
+
+// // Register event listener
+// mediaQuery.addListener(handleTabletChange);
+
+// // Initial check
+// handleTabletChange(mediaQuery);
+
+// nav button
+
+const navButton = document.getElementsByClassName("nav-button");
+const navBar = document.getElementsByClassName("navbar");
+const toggleNav = () => {
+  console.log(navBar[0]);
+  navBar[0].classList.toggle("hide__navbar");
+};
+
+navButton[0].addEventListener("click", toggleNav);
+
+// To home button
+
+// window.addEventListener("scroll"), () =>{
+//   let page
+// }
+
+// parallax scroll and nav hoe button
+const fixedHomeButton = document.getElementById("fixedbutton");
+const parallax = document.getElementById("parallax");
+const parallax2 = document.getElementById("parallax2");
+
+window.addEventListener("scroll", () => {
+  let pageOffset = window.pageYOffset;
+  if (pageOffset < 175) {
+    console.log("less");
+    fixedHomeButton.classList.add("hide__fixed__button");
+  } else {
+    fixedHomeButton.classList.remove("hide__fixed__button");
+  }
+  console.log(pageOffset);
+  parallax.style.backgroundPositionY = pageOffset * 0.1 + "px";
+  parallax2.style.backgroundPositionY = pageOffset * 0.05 + "px";
+});
